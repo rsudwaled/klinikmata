3e17655bf9cbc753d331bf7e238387aa62ae0f98
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2023-01-12 10:16:27 +0700

    init commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function login(Request $request)
+    {
+        $input = $request->all();
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+        $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        $user = User::where('username', $request->username)
+            ->orWhere('email', $request->username)->first();
+        if (isset($user)) {
+            if (empty($user->email_verified_at)) {
+                return view('vendor.adminlte.auth.verify', compact(['request', 'user']));
+            }
+        } else {
+            return redirect()->route('login')->withErrors('Username / Email Tidak Ditemukan');
+        }
+        if (auth()->attempt(array($fieldType => $input['username'], 'password' => $input['password']))) {
+            return redirect()->route('home');
+        } else {
+            return redirect()->route('login')->withErrors('Username / Email dan Password Salah');
+        }
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BPJS\Antrian\PoliklinikAntrian;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        $user = User::create([
+            "name" => "Admin IT",
+            "email" => "adminit@gmail.com",
+            "username" => "adminit",
+            "phone" => "089529909036",
+            'password' => bcrypt('qweqwe123'),
+            'user_id' => 2,
+            'email_verified_at' => now()
+
+        ]);
+        $user->assignRole('Admin');
+        $user = User::create([
+            "name" => "Admin Super",
+            "email" => "adminrs@gmail.com",
+            "username" => "adminrs",
+            "phone" => "089529909036",
+            'password' => bcrypt('qweqwe123'),
+            'user_id' => 2,
+            'email_verified_at' => now()
+        ]);
+        $user->assignRole('Admin Super');
+        $user = User::create([
+            "name" => "Marwan Dhiaur Rahman",
+            "email" => "marwandhiaurrahman@gmail.com",
+            "username" => "marwan",
+            "phone" => "089529909036",
+            'password' => bcrypt('qweqwe123'),
+        ]);
+        $user->assignRole('Admin Super');
+        $user = User::create([
+            "name" => "Admin Antrian BPJS",
+            "email" => "antrianbpjs@gmail.com",
+            "username" => "antrianbpjs",
+            "phone" => "089529909036",
+            'password' => bcrypt('antrianbpjs'),
+            'user_id' => 2,
+            'email_verified_at' => now()
+        ]);
+        $user->assignRole('Admin Super');
+
+        $user = User::create([
+            "name" => "Admin Pendaftaran",
+            "email" => "adminpendaftaran@gmail.com",
+            "username" => "adminpendaftaran",
+            "phone" => "089529909036",
+            'password' => bcrypt('adminpendaftaran'),
+        ]);
+        $user->assignRole('Pendaftaran');
+        $user = User::create([
+            "name" => "Admin Kasir",
+            "email" => "adminkasir@gmail.com",
+            "username" => "adminkasir",
+            "phone" => "089529909036",
+            'password' => bcrypt('adminkasir'),
+        ]);
+        $user->assignRole('Kasir');
+        $user = User::create([
+            "name" => "Admin Poliklinik",
+            "email" => "adminpoli@gmail.com",
+            "username" => "adminpoli",
+            "phone" => "089529909036",
+            'password' => bcrypt('adminpoli'),
+        ]);
+        $user->assignRole('Poliklinik');
+        $user = User::create([
+            "name" => "Admin Farmasi",
+            "email" => "adminfarmasi@gmail.com",
+            "username" => "adminfarmasi",
+            "phone" => "089529909036",
+            'password' => bcrypt('adminfarmasi'),
+        ]);
+        $user->assignRole('Farmasi');
+        $user = User::create([
+            "name" => "Admin Pelayanan Medis",
+            "email" => "adminyanmed@gmail.com",
+            "username" => "adminyanmed",
+            "phone" => "089529909036",
+            'password' => bcrypt('adminyanmed'),
+        ]);
+        $user->assignRole('Pelayanan Medis');
+        $user = User::create([
+            "name" => "Admin Rekam Medis",
+            "email" => "adminrekammedis@gmail.com",
+            "username" => "adminrekammedis",
+            "phone" => "089529909036",
+            'password' => bcrypt('adminrekammedis'),
+        ]);
+        $user->assignRole('Rekam Medis');
+    }
+}
