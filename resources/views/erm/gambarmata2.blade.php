ae90b53a4d512b5e1e225d1ae60e11dc948d286f
apw516 <redhawk5665@gmail.com>
2023-01-21 10:54:35 +0700

    21 januari 2023, penandaan gambar

---
+++ resources/views/erm/gambarmata2.blade.php
@@ -0,0 +1,35 @@
+<script src="{{ asset('vendor/marker/markerjs2_2.js') }}"></script>
+<input hidden type="text" id="gambarmata2">
+<img id="gambarnya2" style="margin-top:50px" width="400px" src="{{ asset('klinik mata losari/bolamata2.jpg') }}"
+    onclick="showMarkerArea(this);" />
+<canvas hidden id="myCanvas2" width="500px" height="450px" style="border:1px solid #d3d3d3;">
+    Your browser does not support the HTML5 canvas tag.
+</canvas>
+<button class="btn btn-danger mt-2" onclick="batalgambar2()">batal</button>
+
+<script>
+    function showMarkerArea(target) {
+        const markerArea = new markerjs2.MarkerArea(target);
+        markerArea.addEventListener("render", (event) => (target.src = event.dataUrl));
+        markerArea.show();
+    }
+    function batalgambar2() {
+        ambilgambar2()
+    }
+    function ambilgambar2() {
+            $.ajax({
+                type: 'post',
+                data: {
+                    _token: "{{ csrf_token() }}",
+                },
+                url: '<?= route('gambarmata2') ?>',
+                error: function(data) {
+                    alert('ok')
+                },
+                success: function(response) {
+                    spinner.hide()
+                    $('.gambar2').html(response)
+                }
+            });
+        }
+</script>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,5 +1,21 @@
-@section('plugins.DateRangePicker', true)
-@section('plugins.Datatables', true)
-@section('plugins.Select2', true)
-@section('plugins.Sweetalert2', true)
-@section('plugins.TempusDominusBs4', true)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>All defaults demo</title>
+  <script src="https://unpkg.com/markerjs2/markerjs2.js"></script>
+  <script>
+    function showMarkerArea(target) {
+      const markerArea = new markerjs2.MarkerArea(target);
+      markerArea.addEventListener("render", (event) => (target.src = event.dataUrl));
+      markerArea.show();
+    }
+  </script>
+</head>
+<body>
+
+  <img src="{{ asset('klinik mata losari/klinik mata losari.jpg }}" style="max-width: 600px;" onclick="showMarkerArea(this);" />
+
+</body>
+</html>
